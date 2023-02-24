aa21f137270adc2b18ff6f50ca66c56d1d53084f
Krithik-Lashwin <125042637+Krithik-Lashwin@users.noreply.github.com>
2023-02-24 18:07:04 +0400

    Add files via upload

---
+++ Times Tables Generator.py
@@ -0,0 +1,4 @@
+print('This is a program to generate multiplication tables')
+n = int(input("Enter the number for printing out its table"))
+for i in range(0,13):
+    print(n, " × ", i, " = ", i*n)
---
+++ While Loop(independent).py
@@ -0,0 +1,5 @@
+n = int(input("Enter the number"))
+i = 0
+while i < 13:
+    print(n, " × ", i, " = ", n*i)
+    i += 1
